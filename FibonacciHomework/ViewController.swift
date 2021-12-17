b96e0efa1c7cfdbb24360ad856f27016ca8a8a65
Gökalp Cimin <88550831+gokalpcimin@users.noreply.github.com>
2021-12-17 20:42:37 +0300

    Update ViewController.swift

--- FibonacciHomework/ViewController.swift
+++ FibonacciHomework/ViewController.swift
@@ -8,6 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    import UIKit
+
+var number1 = 0
+var number2 = 1
+var number3 = 0
+var range1 = 750
+var range2 = 1000
+var fibNubs = [Int]()
+
+print("\(range1) ve \(range2) arasındaki Fibonacci Sayıları")
+
+while (number3<range2) {
+    number3 = number1 + number2
+    number1 = number2
+    number2 = number3
+   
+    if number3 > range1 && number3 < range2 {
+        fibNubs.append(number3)
+     
+        
+    }else{
+        
+    continue
+    }
+}
+print(fibNubs)
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
